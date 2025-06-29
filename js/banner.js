33964073249c8fd43b43be3bf132104765b98436
dev.clarova <dev.clarova@gmail.com>
2025-06-30 00:15:25 +0900

    [docs] 헤더 및 배너 진행/배너 문제 해결 필요

---
+++ js/banner.js
@@ -0,0 +1,39 @@
+window.addEventListener("load", function () {
+  // swiper 만들기 실행
+  makeSlide();
+});
+
+// 슬라이드 만들기
+function makeSlide() {
+  const swiper = new Swiper(".banner_info", {
+    slidesPerView: "auto",
+    spaceBetween: 85,
+    centeredSlides: true,
+    loop: true,
+    loopedSlides: 8,
+    speed: 1000,
+    autoplay: {
+      delay: 1000,
+      disableOnInteraction: false,
+    },
+    navigation: {
+      nextEl: ".banner_next",
+      prevEl: ".banner_prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+      760: {
+        slidesPerView: 1,
+        spaceBetween: 25,
+      },
+    },
+  });
+  return swiper;
+}
+
+makeHtml();
+
+const swiper = makeSlide();
