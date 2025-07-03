1cca43b90bf809df649f3d6f17c3c3b0ff2e5baa
dev.clarova <dev.clarova@gmail.com>
2025-07-04 00:48:35 +0900

    [docs] 헤더 js 및 hot_notice 진행

--- js/header.js
+++ js/header.js
@@ -1,5 +1,20 @@
-window.addEventListener("DOMContentLoaded", () => {
+window.addEventListener("DOMContentLoaded", function () {
+  const header_menu = document.querySelector(".header_menu");
+  const menus = header_menu.querySelectorAll(".menu");
 
-    const ApiData =
-    
+  menus.forEach((menu) => {
+    menu.addEventListener("mouseenter", function () {
+      menus.forEach((otherMenu) => {
+        if (otherMenu !== menu) {
+          otherMenu.style.color = "rgb(170, 170, 170)";
+        } else {
+          otherMenu.style.opacity = "#000";
+        }
+      });
+    });
+
+    menu.addEventListener("mouseleave", function () {
+      menus.forEach((m) => (m.style.color = "#000"));
+    });
+  });
 });
